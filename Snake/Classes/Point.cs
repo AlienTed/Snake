196da4c0d11053fe813aa316abea943c05b1c8de
Vladimir Bogachev <vladimir_bogachev@yahoo.com>
2019-11-11 19:16:28 +0300

    Added Point class

---
+++ Snake/Classes/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake.Classes
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symbol;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
